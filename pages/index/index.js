8f8173ef854b5869b3856c3bef73e70221d64f63
superman31415926 <285440706@qq.com>
2020-09-07 22:34:29 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,38 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    var that = this;
+    wx.request({
+      url: 'http://cc.cc/index/index/index',
+      success (e){
+        that.setData({
+          banner : e.data.result
+        })
+        console.log(that.data.banner);
+      }
+    })
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
