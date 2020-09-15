58306272d623b083009eb7907fc8e32aa4326d0f
superman31415926 <285440706@qq.com>
2020-09-15 09:52:53 +0800

    vehicle inspection

--- pages/market/market.js
+++ pages/market/market.js
@@ -1,42 +1,6 @@
 // pages/makelist/makelist.js
 Page({
     data: {
-      category: [{ "name": "机油" }, { "name": "润滑油" }, { "name": "变速邮箱" }, { "name": "刹车油" }, { "name": "饰品" }, { "name": "超市" }, { "name": "更多" }],
-      dataList:[
-        {
-          name: '南极人座椅靠背',
-          img: '/../images/goods1.jpg'
-        },
-        {
-          name: '后视镜倒车雷达',
-          img: '/../images/goods2.jpg'
-        },
-        {
-          name: '车掸',
-          img: '/../images/goods3.jpg'
-        },
-        {
-          name: 'BOZL水蜡',
-          img: '/../images/goods4.jpg'
-        },
-        {
-          name: '零配件',
-          img: '/../images/goods5.jpg'
-        },
-        {
-          name: '后视镜倒车雷达',
-          img: '/../images/goods2.jpg'
-        },
-        {
-          name: '车掸',
-          img: '/../images/goods3.jpg'
-        }
-      ],
-      banner:[
-        {img:'/../images/market1.png'},
-        {img: '/../images/market2.jpg'},
-        {img: '/../images/market3.jpg'}
-      ],
     // 切换class样式标志
         flag:0
       },
